4492cd5c964c61a2b51a4019857c21425dc4e361
Julia-Ko-0 <117535623+Julia-Ko-0@users.noreply.github.com>
2023-01-30 21:18:02 +0300

    Add files via upload

---
+++ dz2.js
@@ -0,0 +1,4 @@
+x = 23
+let x1 = Math.floor(x/10)
+let x2 = (x - x1*10)
+console.log(x2,x1)
---
+++ dz4.js
@@ -0,0 +1,7 @@
+let x = 12
+let y = 17
+let z = 8
+let sra = (Math.abs(x)**3+Math.abs(y)**3+Math.abs(z)**3)/3
+console.log ('Среднее арифметич кубов = ',sra)
+let geom = (Math.abs(x)**2*Math.abs(y)**2*Math.abs(z)**2)**(1/3)
+console.log ('Среднее геометрическое = ', geom)
