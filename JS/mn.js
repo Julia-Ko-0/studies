fd44e547f9b354af4031ab69277863bcf87f350a
Julia <123kochygova@gmail.com>
2023-03-15 20:22:10 +0300

    f

--- JS/mn.js
+++ JS/mn.js
@@ -1,8 +1,39 @@
-let arr = ["Я", "изучаю", "JavaScript"];
+let name = ['Anton','Blada','Ksusha','Dima']
+let evaluations = [50,70,85,100]
 
-// с индекса 2
-// удалить 0 элементов
-// вставить "сложный", "язык"
-arr.splice(2, 0, "сложный", "язык");
 
-console.log( arr ); // "Я", "изучаю", "сложный", "язык", "JavaScript"
+
+let i = 0
+
+
+
+for  (i in evaluations) {
+
+    if  (evaluations [i]<60){
+        let otv = "F"
+        console.log(name [i], otv )    
+    }
+    else if (evaluations [i]<70){
+        let otv = "D"
+        console.log(name [i], otv )
+    }
+    else if (evaluations [i]<80){
+        let otv = "C"
+        console.log(name [i], otv )
+    }
+    else if (evaluations [i]<90){
+        let otv = "B"
+        console.log(name [i], otv )
+    }
+    else if(evaluations[i]<=100){
+        let otv = "A"
+        console.log(name[i], otv )
+    }
+    
+
+}
+let sum =0
+for (let a = 0; a< evaluations.length;a++){
+    sum += evaluations[a]
+}
+console.log(sum/(evaluations).length)
