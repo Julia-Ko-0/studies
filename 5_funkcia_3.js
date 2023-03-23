c9ab8416ac07a574ed054b3c7970f541ce1dbb71
Julia <123kochygova@gmail.com>
2023-03-23 20:15:53 +0300

    obj

---
+++ 5_funkcia_3.js
@@ -0,0 +1,28 @@
+// function ochishMas(mas){
+//     if(mas !== false && mas !== undefined && mas !== " " && mas !== 0 && mas !== null){
+//        let newMas = []
+//        newMas.push(mas) 
+//     }
+// }
+
+var mas = [4," ",null, false , 2,12,24,0,-32,-1,-1,34]
+for (i=0; i < mas.length ; i++){
+    ochishMas(mas[i])
+}
+console.log(mas)
+
+
+function ochishMas(mas){
+    if(mas !== false && mas !== undefined && mas !== " " && mas !== 0 && mas !== null){
+      
+    }
+}
+
+// const array = [1, null, "2", null, null, 3.0]
+// const array2 = array.filter(ochishMas);
+// console.log(array);
+
+
+// const array = [1, null, "2", null, null, 3.0]
+// const array2 = array.filter(element => element !== null);
+// console.log(array2);
