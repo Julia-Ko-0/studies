ebd663230606c168eaff050242294c70f4f1e28d
Julia K <123kochygova@gmail.com>
2023-04-03 15:15:48 +0300

    jk

---
+++ JS/lb6_1.js
@@ -0,0 +1,13 @@
+let objects = [
+    { name: Василий, surname:Васильев },
+    { name:Иван, surname:Иванов},
+    { name:Пётр, surname: Петров }
+]
+function filter(nzObg,nzSv,Svoistvo){
+    for(let i in nzObg){
+        if(nzObg[i].nzSv = Svoistvo){
+            console.log(nzObg[i])
+        }
+
+    }
+}
