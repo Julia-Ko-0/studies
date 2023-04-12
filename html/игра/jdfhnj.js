b54d9c14ea9466bf02ffd3b952b819ad65b04595
Julia <123kochygova@gmail.com>
2023-04-12 20:02:44 +0300

    cngxfgcjn

--- html/игра/jdfhnj.js
+++ html/игра/jdfhnj.js
@@ -1,41 +1,7 @@
-// function createElement(mas1){
+cardsArray = [1,2,3,4,5]
 
-   
-//     // let chislo = Math.floor( Math.random() * (4 - 1)+1)
-//     while(mas1.length <4  ){
-//         let chislo = Math.floor( Math.random() * (5 - 1)+1)
-        
-//         if(mas1.includes(chislo) == true){
-            
-//         }
-//         else{
-//             mas1.push(chislo)
-//             console.log(mas1 , mas1.length)
-//         }
-       
-//     }
-//     console.log(mas1)
-// }
-function createElement(mas1,mas2){
+let gameGrid = cardsArray.concat(cardsArray).sort(function () {
+    return  Math.random()-0.5 ;
+  });
   
-    // let chislo = Math.floor( Math.random() * (4 - 1)+1)
-    while(mas1.length<4 && mas2.length<4 ){
-        let chislo = Math.floor( Math.random() * (4 - 1)+1)
-        if(mas1.includes(chislo) !== true){
-            mas1.push(chislo)
-        }
-        else if (mas2.includes(chislo) !== true){
-            mas2.push(chislo)
-        }    
-    }
-    // while(mas1.length<4 && mas2.lastIndexOf<4 ){
-    //     let chislo = Math.floor( Math.random() * (4 - 1)+1)
-    //     if(mas2.includes(chislo)){
-    //     }
-    //     else {
-    //         mas2.push(chislo)
-    //     }    
-    // }
-
-    console.log(mas1,mas2)
-}
+  console.log(gameGrid)
