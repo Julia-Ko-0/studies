bc4392ea61710096e7807948c851141404f5c0c2
Julia K <123kochygova@gmail.com>
2023-06-05 21:05:19 +0300

    hgjhgghj

--- blog/script.js
+++ blog/script.js
@@ -17,7 +17,7 @@ let ul = document.getElementById('ul')
 //     // .then((response)=>response.json())
 //     // .then((app)=> console.log(app))
 // }
-let ids
+
 async function appServer(){
     const response = await fetch("https://gorest.co.in/public-api/posts?page=10",{
         method:"GET"
@@ -28,7 +28,9 @@ async function appServer(){
     console.log('aaaa')
     for(let i = 0;i< 100;i++){
         console.log('aaaa')
+        // ids = date.data[i].id
         let elem = createBlog(date.data[i].title,date.data[i].id)
+        
         
       
       
@@ -38,8 +40,13 @@ async function appServer2(id){
     const response = await fetch(`https://gorest.co.in/public-api/posts/${id}`,{
         method:"GET"
     })
-    .then((response)=>response.json())
-    .then((app)=> console.log(app))
+    const date = await response.json()
+    console.log(date)
+    console.log(date.body)
+
+    createBlog2(date.data.title, date.data.body)
+    // .then((response)=>response.json())
+    // .then((app)=> createBlog(app,id))
     
 }
 function createBlog(name,id){
@@ -49,11 +56,19 @@ function createBlog(name,id){
     p.href = "index_2.html"
     li.append(p)
     ul.append(li)
-    // p.addEventListener("click",()=>{
-    //     ids
-    //     console.log(ids)
-    // })
-    return id
+    p.addEventListener("click",()=>{
+        sessionStorage.setItem('ids', id)
+    })
+    
+
+}
+function createBlog2(title,bady){
+    let h1 = document.createElement("h1")
+    let body = document.createElement("h2")
+    h1.textContent = title
+    body.textContent = bady
+    ul.append(h1)
+    ul.append(body)
 
 }
 function appBlog(){
@@ -61,6 +76,7 @@ function appBlog(){
     //     createServer(mas[i],owner)
     // }
     appServer()
+
     // if(owner = 2){
     //     appServer2(cr.id)
     // }
@@ -70,9 +86,10 @@ function appBlog(){
     
 }
 function appBlog2(){
-    
-    let cr = createBlog()
-    console.log(cr.id)
+    console.log(sessionStorage.getItem('ids'))
+    appServer2(sessionStorage.getItem('ids'))
+    // let cr = createBlog()
+    // console.log(cr.id)
     
     
 }
