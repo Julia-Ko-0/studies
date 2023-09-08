fb5198a8fdb1237f15a961c7fc94f2e01938c6b6
Julia K <123kochygova@gmail.com>
2023-09-08 09:46:28 +0300

    фыавф

---
+++ материал для работы copy/js/main.js
@@ -0,0 +1,12 @@
+const butAut = document.querySelector('.button-auth')
+const modalAut = document.querySelector('.modal-auth')
+ 
+butAut.addEventListener('click',() => {
+    modalAut.style.display = 'flex'
+    console.log('drg')
+})
+let btnClose = document.querySelector(".close-auth")
+btnClose.addEventListener('click',() =>{
+    console.log('aaaaaaa')
+ 
+})
